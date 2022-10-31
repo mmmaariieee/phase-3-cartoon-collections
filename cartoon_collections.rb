262a8b53844e3913ff923056a8898358c0c5bd0a
Mariia <m.goncharenkoo@icloud.com>
2022-10-31 10:02:15 -0400

    commit tree 02c14c1327fc6192e33e6b15f396a95ac09c017a

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,26 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves (dwarves)
+  dwarves.each.with_index do |dwarf, i|
+    i += 1
+    puts "#{i}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet (planeteer_calls)
+  planeteer_calls.map do |planet|
+    "#{planet.capitalize}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls (planeteer_calls)
+  planeteer_calls.any? do |planet|
+    planet.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese (array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  array.find do |maybe_cheese|
+    cheese_types.include? (maybe_cheese)
+  end
 end
